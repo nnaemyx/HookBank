d4468e572356cd4203d136cce533098c8652abfc
tezon <tezonteam@gmail.com>
2022-09-09 16:43:28 -0700

    toast

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -3,8 +3,8 @@ import {FcCheckmark} from 'react-icons/fc'
 
 function Toast() {
   return (
-    <div className="flex border p-2 gap-2 border-none bg-black">
-      <FcCheckmark size={25}/>
+    <div className="flex border p-2 gap-2 border-none bg-green-900">
+      <FcCheckmark size={30}/>
       <h1 className="text-white text-[20px]">
         Registration successful
       </h1>
